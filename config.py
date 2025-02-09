63c2731f1419b1c501d143e0c236be5266c39e43
Shanksdb <akashmadness109@gmail.com>
2025-02-09 21:14:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+
 from os import environ
 
 API_ID = int(environ.get("API_ID", "28713982"))
@@ -6,7 +7,9 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "-1002100963256"))
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002100963256"))
-ADMINS = int(environ.get("ADMINS", "7195990500 5512860435"))
+
+# Split the space-separated string into a list of integers
+ADMINS = list(map(int, environ.get("ADMINS", "7195990500 5512860435").split()))
 
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_URI = environ.get("DB_URI", "")
